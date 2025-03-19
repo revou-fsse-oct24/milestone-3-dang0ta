cbd9729ca0d4e1336e3169fc564881611ec0b81e
dang0ta <ikosiriki@gmail.com>
2025-03-19 23:19:48 +0700

    update transaction routes to use sql db instead of in-memory

--- routes/transactions.py
+++ routes/transactions.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, request, jsonify
 from pydantic import ValidationError
 from auth_jwt import jwt_required, get_jwt_identity
-from db_inmemory import TransactionRepository, TransactionQuery, NewTransactionRequest
+from db.transactions import withdraw, deposit, transfer, get_transactions as db_get_transactions, get_transaction as db_get_transaction, TransactionNotFoundException
+from db.accounts import AccountNotFoundException, AccountsNotFoundException
+from models import Transaction
 
-def transaction_bp(transactions: TransactionRepository) -> Blueprint:
+def transaction_bp() -> Blueprint:
     bp = Blueprint("transactions", __name__, url_prefix="/transactions")
 
     @bp.route("/", methods=["GET", "POST"])
@@ -11,46 +13,54 @@ def transaction_bp(transactions: TransactionRepository) -> Blueprint:
     def handle_root_transaction():
          match request.method:
               case "GET":
-                   return get_transactions(transactions)
+                   return get_transactions()
               case "POST":
-                   return create_transaction(transactions)
+                   return create_transaction()
               case _:
                  return "Method not allowed", 405
         
     @bp.route("/<string:id>", methods=["GET"])
     def get_transaction(id:str):
-        transaction = transactions.find_by_id(id)
-        if transaction == None:
-            return "transaction can't be found", 404
-
-        return jsonify({"transaction": transaction}), 200
+        try:
+            transaction = db_get_transaction(id)
+            return jsonify({"transaction": transaction}), 200
+        except TransactionNotFoundException as e:
+            return jsonify({"message": "transaction can't be found"}), 404
     
     return bp
     
 
-def get_transactions(transactions: TransactionRepository):
+def get_transactions():
     try:
         current_user = get_jwt_identity()
         if current_user == None:
             return "Unauthorized", 401
         
-        query = TransactionQuery()
-        if not request.content_length == None and request.content_length > 0:
-            query = TransactionQuery(**request.get_json())
-
-        user_transactions = transactions.query_transaction(user_id=current_user, query=query)
-        return jsonify({"transactions": list(user_transactions)}), 200
+        transactions = db_get_transactions(user_id=current_user)
+        return jsonify({"transactions": transactions}), 200
     except ValidationError as e:
         return e.errors(), 400
+    except AccountsNotFoundException as e:
+        return jsonify({"message": "no account found for the user"}), 404
 
-def create_transaction(transactions: TransactionRepository):
+def create_transaction():
     try:
         current_user = get_jwt_identity()
         if current_user == None:
             return "Unauthorized",401
         
-        transaction_request = NewTransactionRequest(**request.get_json())
-        created_transaction = transactions.create(transaction_request.create_transaction(user_id=current_user))
-        return jsonify({"transaction": created_transaction}), 201
+        transaction_request = Transaction(**request.get_json())
+        match transaction_request.transaction_type.lower():
+            case "withdraw":
+                transaction = withdraw(transaction_request.account_id, transaction_request.amount)
+                return jsonify({"transaction": transaction}), 200
+            case "deposit":
+                transaction = deposit(transaction_request.account_id, transaction_request.amount)
+                return jsonify({"transaction": transaction}), 200
+            case "transfer":
+                transaction = transfer(account_id=transaction_request.account_id, recipient_account_id=transaction_request.recipient_account_id, amount=transaction_request.amount)
+                return jsonify({"transaction": transaction}), 200
+            case _:
+                return jsonify({"message": "invalid transaction type"}), 400
     except ValidationError as e:
         return e.errors(), 400
