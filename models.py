10451b2389045b2d39a89fa49a408ff9f08b29e7
dang0ta <ikosiriki@gmail.com>
2025-03-29 01:47:19 +0700

    split POST /transactions into different endpoints for withdraw, deposit, and transfer

--- models.py
+++ models.py
@@ -19,7 +19,20 @@ class Account(BaseModel):
     balance: int
 
 class Transaction(BaseModel):
+    id: str = Field(..., description="The ID of the transaction")
     account_id: str = Field(..., description="The ID of the current user's account")
-    recipient_account_id: str | None = Field(None, description="set only if transaction_type is 'transfer', this is the account ID of the recipient account")
     transaction_type: str = Field(..., description="either 'withdraw', 'deposit' or 'transfer'")
+    amount: int = Field(..., description="the nominal of the transfer, accepts only positive number if transfer_type is 'withdraw' or 'deposit'. Negative number on transaction_type transfer indicates transfer from account_id to recipient_account_id, positive number indicates otherwise")
+
+class WithdrawRequest(BaseModel):
+    account_id: str = Field(..., description="The ID of the current user's account")
+    amount: int = Field(..., description="the nominal of the transfer, accepts only positive number if transfer_type is 'withdraw' or 'deposit'. Negative number on transaction_type transfer indicates transfer from account_id to recipient_account_id, positive number indicates otherwise")
+
+class DepositRequest(BaseModel):
+    account_id: str = Field(..., description="The ID of the current user's account")
+    amount: int = Field(..., description="the nominal of the transfer, accepts only positive number if transfer_type is 'withdraw' or 'deposit'. Negative number on transaction_type transfer indicates transfer from account_id to recipient_account_id, positive number indicates otherwise")
+
+class TransferRequest(BaseModel):
+    account_id: str = Field(..., description="The ID of the current user's account")
+    recipient_account_id: str = Field(..., description="The ID of the recipient account")
     amount: int = Field(..., description="the nominal of the transfer, accepts only positive number if transfer_type is 'withdraw' or 'deposit'. Negative number on transaction_type transfer indicates transfer from account_id to recipient_account_id, positive number indicates otherwise")
