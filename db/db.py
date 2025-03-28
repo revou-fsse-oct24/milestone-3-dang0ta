b5df4682773c26494b9d065b90965ddab72fb3b8
dang0ta <ikosiriki@gmail.com>
2025-03-28 17:10:49 +0700

    add configuration check on DB_CONN

--- db/db.py
+++ db/db.py
@@ -1,8 +1,13 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, scoped_session, sessionmaker
+from exceptions import ConfigurationError
 
-engine = create_engine(os.getenv("DB_CONN"), echo=os.getenv('DEBUG', 'False').lower() in ['true', '1', 't'])
+db_conn = os.getenv("DB_CONN")
+if not db_conn:
+    raise ConfigurationError("DB_CONN is not set")
+
+engine = create_engine(db_conn, echo=os.getenv('DEBUG', 'False').lower() in ['true', '1', 't'])
 db_session = scoped_session(sessionmaker(
     autoflush=False, 
     bind=engine,
