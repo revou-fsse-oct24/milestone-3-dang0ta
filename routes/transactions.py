722bc047b40d74be0ec508abc98f72d7069cad27
dang0ta <ikosiriki@gmail.com>
2025-03-29 01:48:13 +0700

    update transaction endpoints

--- routes/transactions.py
+++ routes/transactions.py
@@ -3,45 +3,67 @@ from pydantic import ValidationError
 from auth_jwt import jwt_required, get_jwt_identity
 from db.transactions import withdraw, deposit, transfer, get_transactions as db_get_transactions, get_transaction as db_get_transaction, TransactionNotFoundException
 from db.accounts import AccountNotFoundException, AccountsNotFoundException
-from models import Transaction
+from models import Transaction, WithdrawRequest, DepositRequest, TransferRequest
 
 def transaction_bp() -> Blueprint:
     bp = Blueprint("transactions", __name__, url_prefix="/transactions")
 
-    @bp.route("/", methods=["GET", "POST"])
+    @bp.route("/withdraw", methods=["POST"])
+    @jwt_required
+    def handle_withdraw():
+        try:
+            withdraw_request = WithdrawRequest(**request.get_json())
+            transaction = withdraw(withdraw_request.account_id, withdraw_request.amount)
+            return jsonify({"transaction": transaction.model_dump()}), 200
+        except ValidationError as e:
+            return e.errors(), 400
+        
+    @bp.route("/deposit", methods=["POST"])
+    @jwt_required
+    def handle_deposit():
+        try:
+            deposit_request = DepositRequest(**request.get_json())
+            transaction = deposit(deposit_request.account_id, deposit_request.amount)
+            return jsonify({"transaction": transaction.model_dump()}), 200
+        except ValidationError as e:
+            return e.errors(), 400
+        
+    @bp.route("/transfer", methods=["POST"])
+    @jwt_required
+    def handle_transfer():
+        try:
+            transfer_request = TransferRequest(**request.get_json())
+            transaction = transfer(transfer_request.account_id, transfer_request.recipient_account_id, transfer_request.amount)
+            return jsonify({"transaction": transaction.model_dump()}), 200
+        except ValidationError as e:
+            return e.errors(), 400  
+
+    @bp.route("/")
     @jwt_required
     def handle_root_transaction():
-         match request.method:
-              case "GET":
-                   return get_transactions()
-              case "POST":
-                   return create_transaction()
-              case _:
-                 return "Method not allowed", 405
+        try:
+            current_user = get_jwt_identity()
+            if current_user == None:
+                return "Unauthorized", 401
+            
+            transactions = db_get_transactions(account_id=current_user)
+            return jsonify({"transactions": list(transactions)}), 200
+        except ValidationError as e:
+            return e.errors(), 400
+        except AccountsNotFoundException as e:
+            return jsonify({"message": "no account found for the user"}), 404
         
     @bp.route("/<string:id>", methods=["GET"])
     def get_transaction(id:str):
         try:
             transaction = db_get_transaction(id)
-            return jsonify({"transaction": transaction}), 200
+            return jsonify({"transaction": transaction.model_dump()}), 200
         except TransactionNotFoundException as e:
             return jsonify({"message": "transaction can't be found"}), 404
     
     return bp
     
-
-def get_transactions():
-    try:
-        current_user = get_jwt_identity()
-        if current_user == None:
-            return "Unauthorized", 401
-        
-        transactions = db_get_transactions(user_id=current_user)
-        return jsonify({"transactions": transactions}), 200
-    except ValidationError as e:
-        return e.errors(), 400
-    except AccountsNotFoundException as e:
-        return jsonify({"message": "no account found for the user"}), 404
+    
 
 def create_transaction():
     try:
