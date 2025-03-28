b2c7e335e8a1bd1100717cf8a6ef31da382f5fc6
dang0ta <ikosiriki@gmail.com>
2025-03-28 16:12:32 +0700

    update transaction operations to follow transaction-ledger style

--- db/transactions.py
+++ db/transactions.py
@@ -4,7 +4,7 @@ from models import Transaction as TransactionModel
 from .accounts import AccountNotFoundException, AccountsNotFoundException
 from sqlalchemy import select
 from sqlalchemy.exc import NoResultFound
-from .models import Account, Transaction, User
+from .models import Account, Transactions, TransactionEntries, User
 
 class TransactionNotFoundException(Exception):
     def __init__(self, *, transaction_id:str):
@@ -19,15 +19,22 @@ def withdraw(account_id:str, amount:int) -> Optional[TransactionModel]:
         )
 
         account = db_session.scalars(statement=statement).one()
+        
         account.balance = account.balance - amount
-        transaction = Transaction(
-            amount=amount,
-            account_id=account_id,
-            account=account,
+        transaction = Transactions(
             transaction_type="withdraw"
         )
 
+        entry = TransactionEntries(
+            transaction_id = transaction.id,
+            transaction = transaction,
+            account_id = account_id,
+            account = account,
+            entry_type = "debit"
+        )
+
         db_session.add(transaction)
+        db_session.add(entry)
         db_session.commit()
         return transaction.to_model()
     except NoResultFound:
@@ -44,33 +51,38 @@ def deposit(account_id:str, amount:int) -> Optional[TransactionModel]:
         )
 
         account = db_session.scalars(statement=statement).one()
-        account.balance = account.balance - amount
-        transaction = Transaction(
-            amount=amount,
-            account_id=account_id,
-            account=account,
-            transaction_type="withdraw"
+        
+        account.balance = account.balance + amount
+        transaction = Transactions(
+            transaction_type="deposit"
+        )
+
+        entry = TransactionEntries(
+            transaction_id = transaction.id,
+            transaction = transaction,
+            account_id = account_id,
+            account = account,
+            entry_type = "credit"
         )
 
         db_session.add(transaction)
+        db_session.add(entry)
         db_session.commit()
         return transaction.to_model()
     except NoResultFound:
         db_session.rollback()
         raise AccountNotFoundException(account_id=account_id)
-    except Exception as e:
+    except Exception:
         db_session.rollback()
-        raise e
 
-def transfer(account_id: str, recipient_account_id: str, amount: int) -> Optional[TransactionModel]:
+def transfer(sender_account_id: str, recipient_account_id: str, amount: int) -> Optional[TransactionModel]:
     try:
-
-        account = db_session.scalars(statement=(
+        sender_account = db_session.scalars(statement=(
             select(Account)
-            .where(Account.id.is_(account_id))
+            .where(Account.id.is_(sender_account_id))
         )).one()
 
-        account.balance = account.balance - amount
+        sender_account.balance = sender_account.balance - amount
 
         recipient_account = db_session.scalars(statement=(
             select(Account)
@@ -78,16 +90,30 @@ def transfer(account_id: str, recipient_account_id: str, amount: int) -> Optiona
         )).one()
 
         recipient_account.balance = recipient_account.balance + amount
-        
-        transaction = Transaction(
-            amount=amount,
-            account_id=account_id,
-            recipient_account_id=recipient_account_id,
-            recipient_account=recipient_account,
+
+        transaction = Transactions(
             transaction_type="transfer"
         )
 
+        sender_entry = TransactionEntries(
+            transaction_id = transaction.id,
+            transaction = transaction,
+            account_id = sender_account_id,
+            account = sender_account,
+            entry_type = "debit"
+        )
+
+        recipient_entry = TransactionEntries(
+            transaction_id = transaction.id,
+            transaction = transaction,
+            account_id = recipient_account_id,
+            account = recipient_account,
+            entry_type = "credit"
+        )
+
         db_session.add(transaction)
+        db_session.add(sender_entry)
+        db_session.add(recipient_entry)
         db_session.commit()
         return transaction.to_model()
     except NoResultFound:
@@ -97,23 +123,22 @@ def transfer(account_id: str, recipient_account_id: str, amount: int) -> Optiona
         db_session.rollback()
         raise e
     
-def get_transactions(user_id: str) -> List[TransactionModel]:
+def get_transactions(account_id: str) -> List[TransactionModel]:
     try:
         accounts=db_session.scalars(statement=(
             select(Account)
-            .join(User)
-            .where(User.id.is_(user_id))
+            .where(Account.id.is_(account_id))
         ))
 
         transactions: List[TransactionModel] = []
         for account in accounts:
-            for transaction in account.transactions:
+            for transaction in account.transaction_entries:
                 transactions.append(transaction.to_model())
 
         return transactions
     except NoResultFound:
         db_session.rollback()
-        raise AccountsNotFoundException(user_id=user_id)
+        raise AccountNotFoundException(account_id=account_id)
     except Exception as e:
         db_session.rollback()
         raise e
@@ -121,10 +146,10 @@ def get_transactions(user_id: str) -> List[TransactionModel]:
 def get_transaction(transaction_id:str) -> Optional[TransactionModel]:
     try:
         transaction=db_session.scalars(statement=(
-            select(Transaction)
-            .where(Transaction.id.is_(transaction_id))
+            select(Transactions)
+            .where(Transactions.id.is_(transaction_id))
         )).one()
-
+    
         return transaction.to_model()
     except NoResultFound:
         db_session.rollback()
