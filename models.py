dcbcc296297b110820f82f9dd71986393d7ca19f
dang0ta <ikosiriki@gmail.com>
2025-04-04 17:53:50 +0700

    add explicit request models

--- models.py
+++ models.py
@@ -7,7 +7,9 @@ class UserInformation(BaseModel):
     name: str = Field(..., description="The name of the user")
     fullname: str | None = Field(None, description="The full name of the user")
     email_address: EmailStr = Field(..., description="The email address of the user")
+    default_account_id: str | None = Field(None, description="The id of the default account")
 
+# TODO: delete UserCredential class
 class UserCredential(UserInformation):    
     password: str
 
@@ -17,10 +19,41 @@ class UserCredential(UserInformation):
             fullname=self.fullname,
             email_address=self.email_address
         )
+    
+class CreateUserRequest(BaseModel):
+    name: str = Field(..., description="The name of the user")
+    fullname: str | None = Field(None, description="The full name of the user")
+    email_address: EmailStr = Field(..., description="The email address of the user")
+    password: str = Field(..., description="user's password for authentication")
 
 class Account(BaseModel):
+    id: str
+    user_id: str
+    balance: int
+    created_at: datetime
+    updated_at: datetime
+
+    # there's a strange interaction for sqlalchemy's model and pydantic's model when dealing
+    # with datetime. sqlalchemy serializes its own DateTime into human-readable format like 
+    # 'Fri, April 4th, 2025 ....' and they're not parseable by pydantic. Pydantic also seems to 
+    # do this, so when serializing the model with model_dump(), and then turning it back into 
+    # model class with 'Model(**response)' it gives parsing error on the datetime. I'm probably
+    # missing something on this, but this is the current working aproach that I can find for 
+    # safely serialize and deserialize datetime between pydantic and sqlalchemy model. Notice
+    # that I'm also calling '.isoformat()' on `to_model()` method on sqlalchemy models.
+    @field_serializer('created_at')
+    def serialize_created_at(self, created_at: datetime, _info):
+        return created_at.isoformat()
+    
+    @field_serializer('updated_at') 
+    def serialize_updated_at(self, updated_at: datetime, _info):
+        return updated_at.isoformat()
+class CreateAccountRequest(BaseModel):
     balance: int
 
+class UpdateAccountRequest(BaseModel):
+    account_id: str | None = Field(None, description="the ID of the account that will be updated, if not set, the user's default account will be updated instead")
+    balance: int = Field(..., description="the balance change that will be applied to the account")
 class TransactionTypes(str, Enum):
     model_config = ConfigDict(use_enum_values=True)
     withdraw = "withdraw"
