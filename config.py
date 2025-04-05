befc11d0986dc421700793c5485ca0db025ee4c4
dang0ta <ikosiriki@gmail.com>
2025-04-06 01:42:20 +0700

    replace .env with os.getenv

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
 from dotenv import load_dotenv, dotenv_values
+import os
 
-load_dotenv()
-config = dotenv_values(".env")
-db_conn = config["DB_CONN"]
-jwt_secret = config["JWT_SECRET"]
-jwt_algorithm = config["JWT_ALGORITHM"] or "HS256"
+# load_dotenv()
+# config = dotenv_values(".env")
+# db_conn = config["DB_CONN"]
+# jwt_secret = config["JWT_SECRET"]
+# jwt_algorithm = config["JWT_ALGORITHM"] or "HS256"
+
+db_conn = os.getenv("DB_CONN")
+jwt_secret = os.getenv("JWT_SECRET")
+jwt_algorithm = os.getenv("JWT_ALGORITHM", "HS256")
