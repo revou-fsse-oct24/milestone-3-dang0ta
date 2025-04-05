0aacc324a5fbaddef986f2627fddc0e9f464d86a
dang0ta <ikosiriki@gmail.com>
2025-04-06 00:53:59 +0700

    improve coverage on exceptions.py

---
+++ tests/test_exceptions.py
@@ -0,0 +1,5 @@
+from exceptions import ConfigurationError
+
+def test_configuration_error():
+    exc = ConfigurationError("test")
+    assert str(exc) ==  "Some required configurations are missing or not set properly: test"
