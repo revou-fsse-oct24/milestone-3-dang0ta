efcf438ba40f50caf5ccbb1aaf766abbd7296797
dang0ta <ikosiriki@gmail.com>
2025-04-07 03:07:05 +0700

    add get budget endpoint

--- routes/budgets.py
+++ routes/budgets.py
@@ -3,7 +3,12 @@ from auth_jwt import jwt_required, get_jwt_identity
 from models import CreateBudgetRequest, UpdateBudgetRequest
 from pydantic import ValidationError
 from shared.exceptions import parseValidationError
-from db.budgets import create_budget as db_create_budget, get_budgets as db_get_budgets, update_budget as db_update_budget, delete_budget as db_delete_budget, BudgetsNotFoundException, BudgetNotFoundException
+from db.budgets import create_budget as db_create_budget
+from db.budgets import get_budgets as db_get_budgets
+from db.budgets import update_budget as db_update_budget
+from db.budgets import delete_budget as db_delete_budget
+from db.budgets import get_budget as db_get_budget
+from db.budgets import BudgetsNotFoundException, BudgetNotFoundException
 
 def budget_bp() -> Blueprint:
     bp = Blueprint("budgets", __name__, url_prefix="/budgets")
@@ -16,10 +21,12 @@ def budget_bp() -> Blueprint:
             case "GET":
                 return get_budgets()
             
-    @bp.route("/<string:id>", methods=["PUT", "DELETE"])
+    @bp.route("/<string:id>", methods=["GET", "PUT", "DELETE"])
     @jwt_required
     def handle_budget(id: str):
         match request.method:
+            case "GET":
+                return get_budget(id)
             case "PUT":
                 return update_budget(id)
             case "DELETE":
@@ -46,10 +53,18 @@ def get_budgets():
     except BudgetsNotFoundException as e:
         return jsonify({"error": str(e)}), 404
     
+def get_budget(id: str):
+    try:
+        budget =  db_get_budget(id)
+        return jsonify({"budget": budget.model_dump()})
+    except BudgetNotFoundException as e:
+        return jsonify({"error": str(e)}), 404
+    
+    
 def update_budget(id: str):
     try:
         req = UpdateBudgetRequest(**request.get_json())
-        res = db_update_budget(id=id, request=req)
+        res = db_update_budget(budget_id=id, request=req)
         return jsonify({"budget": res.model_dump()})
     except ValidationError as e:
         if e.title == "UpdateBudgetRequest":
@@ -60,12 +75,7 @@ def update_budget(id: str):
     
 def delete_budget(id: str):
     try:
-        req = UpdateBudgetRequest(**request.get_json())
-        db_delete_budget(id=id, request=req)
+        db_delete_budget(budget_id=id)
         return jsonify({"result": "deleted"})
-    except ValidationError as e:
-        if e.title == "UpdateBudgetRequest":
-            return parseValidationError(e, 400)
-        return jsonify({"error": "the server responded with invalid data"}), 500
     except BudgetNotFoundException as e:
         return jsonify({"error": str(e)}), 404
